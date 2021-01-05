558a97169afaaab33b56294cf0644bbddee0b178
Antonio <atcarter005@gmail.com>
2021-01-05 01:01:00 -0600

    commit tree 936969f1ed23f918c8ea922eb910805762e38d2f

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,35 @@
 # code here!
+
+class School
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def roster
+        @roster
+    end
+
+    def add_student(name, grade)
+        if @roster.has_key?(grade)
+            @roster[grade].push(name)
+
+        else
+            @roster[grade] = []
+            @roster[grade] << name
+        end
+
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        @roster.each_value {|value| value.sort!}
+        @roster
+    end
+
+
+end
